04d1dd2d9a4f45294fe44d8ea1fe286e79347f7f
gildgzmn <alexandergildg@gmail.com>
2024-07-08 13:43:40 +0800

    not necessary ??

--- test/app/Models/Cart.php
+++ test/app/Models/Cart.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Cart extends Model
 {
     use HasFactory;
+
+
+    public function cartItems() {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function removeItem($itemId)
+    {
+        return $this->hasOne(CartItem::class)->where('id', $itemId)->delete();
+    }
 }

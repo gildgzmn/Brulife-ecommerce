eab202eed36c67771299563c52345e27dc1ea6cf
gildgzmn <alexandergildg@gmail.com>
2024-07-08 13:38:54 +0800

    relationship to user and product

--- test/app/Models/CartItem.php
+++ test/app/Models/CartItem.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+        'status',
+    ];
+
+    public function user()
+    {
+        //Retrieves the associated User model
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        //Retrieves the associated Products model
+        return $this->belongsTo(Products::class);
+    }
 }

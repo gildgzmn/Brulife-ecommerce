3e53595708f233df3d30c7ad1ee55cd6c2cf4e1c
gildgzmn <alexandergildg@gmail.com>
2024-07-09 16:01:22 +0800

    index json

--- test/app/Http/Controllers/ProductController.php
+++ test/app/Http/Controllers/ProductController.php
@@ -7,6 +7,11 @@ use Inertia\Inertia;
 
 class ProductController extends Controller
 {
+    public function index(){
+
+        $products = Products::all();
+        return response()->json(['products' => $products ]);
+    }
     public function show($id)
     {
         //Retrieves a product by its id from the database

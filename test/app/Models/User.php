00f320640ed5a5f5e63884c26c08db1d5bc5eaed
gildgzmn <alexandergildg@gmail.com>
2024-07-10 16:22:34 +0800

    hasapitokens

--- test/app/Models/User.php
+++ test/app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\CartItem;
+use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.

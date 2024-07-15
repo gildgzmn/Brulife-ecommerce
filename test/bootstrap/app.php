62e85186ee2ccd9ece8251622c3e73ca1127095b
gildgzmn <alexandergildg@gmail.com>
2024-07-15 16:50:39 +0800

    cors mdw idk

--- test/bootstrap/app.php
+++ test/bootstrap/app.php
@@ -3,9 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        //api: __DIR__.'/../routes/api.php', // Ensure API routes are included
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -15,7 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->api(append: [
+            \App\Http\Middleware\CorsMiddleware::class, // Add Custom CORS middleware for API routes
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

ed30dc395ce09ec70937e2d01944edc4030c5f7b
gildgzmn <alexandergildg@gmail.com>
2024-07-15 16:52:18 +0800

    login

--- test/app/Http/Controllers/LoginController.php
+++ test/app/Http/Controllers/LoginController.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Services\LoginService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class LoginController extends Controller
@@ -14,15 +14,15 @@ class LoginController extends Controller
     {
         $this->loginService = $loginService;
     }
-    public function login(Request $request){
-        try{
-            $user = $this->loginService->validateLogin($request->all());
-            return response()->json(['user' => $user ]);
 
-        } catch(\Exception $e){
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
 
+        if (Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Login successful']);
         }
 
-
+        return response()->json(['message' => 'Login failed'], 401);
     }
 }

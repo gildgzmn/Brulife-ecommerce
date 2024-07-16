3900e0ca0968d62a8160fd90b38d1a4ed4ec2f85
gildgzmn <alexandergildg@gmail.com>
2024-07-16 16:27:58 +0800

    nyork

--- test/database/migrations/2024_07_09_081419_create_orders_table.php
+++ test/database/migrations/2024_07_09_081419_create_orders_table.php
@@ -14,9 +14,8 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('shipping_address');
-            $table->string('payment_method');
             $table->decimal('total_price', 10, 2);
+            $table->string('status')->default('pending');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');

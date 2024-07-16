9ee23727d5cf703b80e37729de4cf7147d46f6d0
gildgzmn <alexandergildg@gmail.com>
2024-07-16 16:26:43 +0800

    new order model

--- test/app/Models/OrderItem.php
+++ test/app/Models/OrderItem.php
@@ -17,7 +17,7 @@ class OrderItem extends Model
 
     public function order()
     {
-        return $this->belongsTo(Orders::class);
+        return $this->belongsTo(Order::class);
     }
 
     public function product()

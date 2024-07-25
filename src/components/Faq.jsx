e6dfd17282982edb5091f04ac122e9e36778dd9b
JiroBiag <andreibiag3@gmail.com>
2024-07-25 16:54:51 +0800

    bigger

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,18 +4,18 @@ import { FaChevronRight } from "react-icons/fa";
 
 const FAQSection = () => {
   return (
-    <div className="w-full max-w-md mx-auto mt-32 mb-64" style={{fontSize: '1.05rem'}}>
-      <h2 className="text-xl font-semibold mb-4" style={{fontSize: '1.65rem'}}>FAQ's</h2>
+    <div className="w-full max-w-lg mx-auto mt-32 mb-64" style={{ fontSize: '1.1rem' }}>
+      <h2 className="text-2xl font-semibold mb-6" style={{ fontSize: '1.75rem' }}>FAQ's</h2>
       <Accordion type="multiple">
         {/* FAQ item about placing an order */}
         <AccordionItem value="order">
           <AccordionTrigger>
-            <div className="flex items-center">
+            <div className="flex items-center p-4 text-xl font-medium">
               How to Place an Order
             </div>
           </AccordionTrigger>
           <AccordionContent>
-            <ol className="list-decimal list-inside pl-4" style={{fontSize: '1.00rem'}}>
+            <ol className="list-decimal list-inside pl-6" style={{ fontSize: '1.15rem' }}>
               <li>Add some item(s) into your Shopping Bag. Select the size, color, or quantity if necessary.</li>
               <li>Check out when item selection is completed. Click the VIEW BAG button to review the item(s) selected and make some modifications if necessary.</li>
               <li>Log in to your Account.</li>
@@ -27,12 +27,12 @@ const FAQSection = () => {
         {/* FAQ item about the shopping cart */}
         <AccordionItem value="cart">
           <AccordionTrigger>
-            <div className="flex items-center">
+            <div className="flex items-center p-4 text-xl font-medium">
               Shopping Cart
             </div>
           </AccordionTrigger>
           <AccordionContent>
-            <ol className="list-decimal list-inside pl-4" style={{fontSize: '1.00rem'}}>
+            <ol className="list-decimal list-inside pl-6" style={{ fontSize: '1.15rem' }}>
               <li>Add some item(s) into your Shopping Bag. Select the size, color, or quantity if necessary.</li>
               <li>Check out when item selection is completed. Click the VIEW BAG button to review the item(s) selected and make some modifications if necessary.</li>
               <li>Log in to your Account.</li>

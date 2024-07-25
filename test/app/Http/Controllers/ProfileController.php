1c8d4318c8859d35c77622b08c8070be540cf7fa
gildgzmn <alexandergildg@gmail.com>
2024-07-25 20:37:43 +0800

    removed unnecessary logic at destroy function

--- test/app/Http/Controllers/ProfileController.php
+++ test/app/Http/Controllers/ProfileController.php
@@ -47,19 +47,8 @@ class ProfileController extends Controller
 
     public function destroy(Request $request)
     {
-        $request->validate([
-            'password' => ['required', 'current_password'],
-        ]);
-
         $user = $request->user();
-
-        Auth::logout();
-
         $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
         return response()->json([
             'message' => 'Account deleted successfully',
         ]);

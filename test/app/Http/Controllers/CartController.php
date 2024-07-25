3714aadd041df7bbf8d7551cf7239f3f9a1fb89a
gildgzmn <alexandergildg@gmail.com>
2024-07-25 20:36:42 +0800

    changed the cart index logic

--- test/app/Http/Controllers/CartController.php
+++ test/app/Http/Controllers/CartController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\CartItem;
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function index()
-    {
-        $userId = Auth::user()->id;
-        $cartItems = CartItem::where("user_id", $userId)->get();
-        return response()->json(['cartItems' => $cartItems], 200);
+{
+    if (Auth::check()) {
+        $user = Auth::user();
+        $userId = $user->id;
+    } else {
+        return response()->json(['message' => 'User not authenticated'], 401);
     }
+
+    $cartItems = CartItem::where("user_id", $userId)->get();
+
+    $cartItems = $cartItems->map(function ($item) {
+        $product = Products::find($item->product_id);
+        $item->product_name = $product->name;
+        $item->product_price = $product->price;
+        $item->product_img = $product->img;
+        return $item;
+    });
+
+    return response()->json(['cartItems' => $cartItems], 200);
+}
 
     public function checkout(Request $request){
 
